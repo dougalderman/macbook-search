312fb3bba96339fe402406a1e109d7c566ded990
Doug Alderman <agnadcp-d1@yahoo.com>
2016-04-15 12:19:58 -0600

    4/15/16 changes morning

--- getMacs.js
+++ getMacs.js
@@ -2,21 +2,10 @@ var casper = require('casper').create();
 var classname = 'reload-' + (new Date().getTime());
    
 
-/* casper.start('http://ksl.com', function() {
-    this.echo('ksl.com: ' + this.getTitle());
-    // Click on Classifieds
-    this.clickLabel('Classifieds', a);
-    this.echo('Classifieds: ' + this.getTitle());
-    // Click on Apple Laptops / Desktops
-    this.clickLabel('Apple Laptops/Desktops', a);
-    this.echo('Apple Laptops/Desktops: ' + this.getTitle());
-}); */
-
 casper.start('http://www.ksl.com/index.php?nid=231&cat=554&category=16', function() { // KSL classifieds, Apple Laptops / Desktops
     this.echo('Apple Laptops / Desktops: ' + this.getTitle());
     
-    // Fill out search elements (input fields, no form tag)
-    
+    // Fill out keyword search field and submit
     this.sendKeys('input[name="keyword"]', 'Macbook pro');
     this.click('input[id="searchSubmit"]');
     this.echo('After keyword submit');
@@ -33,6 +22,8 @@ casper.waitWhileSelector('body.' + classname, function() {
         quality: 75
     });
     this.echo('After screenshot');
+    
+     // Fill out min and max search fields and submit (separate submit for price versus keyword. Keyword search still in place)
     this.sendKeys('input[name="min_priceInput"]', '800');
     this.sendKeys('input[name="max_priceInput"]', '1000');
     this.echo('After price submit');
@@ -50,16 +41,51 @@ casper.waitWhileSelector('body.' + classname, function() {
         quality: 75
     });
     this.echo('After screenshot');
+
+    // Use fetchText for retrieving titles because getElementsInfo.map will create separate array elements for 'more...'
+    var titles_str = this.fetchText('a.listlink'); // titles
+    this.echo('Titles: ' + titles_str); 
+    // Split title into array based on /more.../ regex
+    var titles = titles_str.split(/more.../);
+    // Remove blank last element
+    titles.pop();
+    
+    this.echo('titles.length: ' + titles.length);
+    for (var i = 0; i < titles.length; i++) {
+        this.echo('titles[' + i + ']: ' + titles[i]);
+    }
+    
+    // Use getElementsInfo for retrieving descriptions because it doesn't create separate array element for 'more...'. Correctly maps array. 
+    var descriptions = casper.getElementsInfo('div.adDesc').map(function(e){
+    return e.html;
 }); 
-
-// It happens when they change something...
-casper.evaluate(function(classname){
-  document.body.className += ' ' + classname;
-}, classname);
-
-casper.waitWhileSelector('body.' + classname, function() {
-    this.echo('Titles: ' + this.fetchText('a.listlink')); // title
-    this.echo('Brief Descriptions: ' + this.fetchText('div.adDesc')); // brief product description
+    this.echo('descriptions.length: ' + descriptions.length);
+    for (var i = 0; i < descriptions.length; i++) {
+        // remove everything starting with <a class="listlink" from description.
+        var a_loc = descriptions[i].search('<a class="listlink"');
+        if (a_loc > 0)
+            descriptions[i] = descriptions[i].slice(0, a_loc);
+        this.echo('descriptions[' + i + ']: ' + descriptions[i]);
+    }
+    
+ // Filter through title and description for Macbook Pro because original search filter isn't perfect. If no Macbook Pro in title or description remove from array.
+    for (var i = 0; i < titles.length; i++) {
+        if (!((titles[i].search(/macbook pro/i) !== -1) ||
+           (descriptions[i].search(/macbook pro/i) !== -1))) {
+        // if Macbook Pro not found somwhere in title or description, splice from arrays.
+            this.echo('splicing element: ' + i)
+            titles.splice(i, 1);
+            descriptions.splice(i, 1);
+        }
+    }
+    
+    this.echo('titles.length after splice: ' + titles.length);
+    this.echo('descriptions.length after splice: ' + descriptions.length);
+    for (var i = 0; i < titles.length; i++) {
+        this.echo('titles[' + i + ']: ' + titles[i]);
+        this.echo('descriptions[' + i + ']: ' + descriptions[i]);
+    }
+    
 });
 
 casper.run();
