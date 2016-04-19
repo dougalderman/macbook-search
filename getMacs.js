c6082be5bd79b9cd6e726f1b152c3d1fdc020d77
Doug Alderman <agnadcp-d1@yahoo.com>
2016-04-19 15:33:35 -0600

    4/19/16 changes paging working

--- getMacs.js
+++ getMacs.js
@@ -4,7 +4,6 @@ var casper = require('casper').create(
         logLevel: 'debug'
     } */
 ),
-    classname = 'reload-' + (new Date().getTime()),
     titles = [],
     hrefs = [],
     descriptions = [],
@@ -15,9 +14,16 @@ var casper = require('casper').create(
     donePaging = false,
     curr_url = '';
    
-Array.prototype.extend = function (other_array) {
+Array.prototype.extend = function(other_array) {
     if (other_array.length)
         other_array.forEach(function(v) {this.push(v)}, this);    
+};
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
 }
 
 function getHardDriveSizes(str) {
@@ -25,32 +31,30 @@ function getHardDriveSizes(str) {
         hd_sizes = [], // return array of sizes because author may have multiple hard drives
         hd_gbunits_loc = [],
         start = 0,
-        offset = 0;
-    
+        offset = 0,
+        hd_size_start = 0,
+        hd_size_end = 0;
+
     this.echo('in getHardDriveSize()');
     this.echo('str = ' + str);
     str = str.toLowerCase();
-     
+
     // check for terabyte. If any mention of terabyte, it's big enough.
     if (str.search(/terabyte/) !== -1) {
-        hd_sizes.push(1024); 
+        hd_sizes.push(1024);
         return hd_sizes;
     }
-    
+
     this.echo('after terabyte check');
     // for some reason, string match isn't working for me, so I'm doing a workaround here using search to get all matched elements.
-    while ((offset = str.substr(start).search(/gb|gigabyte|gig|ssd|hhd|hdd/)) !== -1) {   
-        hd_gbunits_loc.push(start + offset);
+    while ((offset = str.substr(start).search(/gb|gigabyte|gig|ssd|hhd|hdd/)) !== -1) {         hd_gbunits_loc.push(start + offset);
         start += offset + 1;
     }   
-    this.echo('hd_gbunits_loc.length = ' + hd_gbunits_loc.length); 
+    this.echo('hd_gbunits_loc.length = ' + hd_gbunits_loc.length);
       
     for (var j = 0; j < hd_gbunits_loc.length; j++) {
-        
         this.echo('j: ' + j);
         this.echo('hd_gbunits_loc: ' + hd_gbunits_loc[j]);
-        var hd_size_start = 0;
-        var hd_size_end = 0;
         hd_size = 0;
         var foundNumber = false;
         for (var k = hd_gbunits_loc[j] - 1; k > 0; k--) {
@@ -89,16 +93,14 @@ function getHardDriveSizes(str) {
         if (hd_size !== 0) { // if nonzero hard drive size in string
             this.echo('pushing ' + hd_size + ' into array');
             hd_sizes.push(hd_size); // add to array of hd_sizes
-        } 
+        }
         
     } // j loop
-    
-     
-    return hd_sizes; 
+
+    return hd_sizes;
 }  // getHardDriveSizes
 
 function getMacInfo() {
-  
     // save off url
     curr_url = this.getCurrentUrl();
     this.echo('curr_url: ' + curr_url);
@@ -110,28 +112,28 @@ function getMacInfo() {
     for (var i = titles_arr.length - 1; i >= 0; i--) {
         // remove 'more...' elements
         if (titles_arr[i] === 'more...')
-            titles_arr.splice(i, 1); 
+            titles_arr.splice(i, 1);
     }
-    
-    this.echo('titles_arr_length after removing more... elements: ' + titles_arr.length); 
-    
+
+    this.echo('titles_arr_length after removing more... elements: ' + titles_arr.length);
+
     for (var i = 0; i < titles_arr.length; i++) {
         this.echo('titles_arr[' + i + ']: ' + titles_arr[i]);
     }
     
     titles.extend(titles_arr);
     this.echo('titles.length after extend: ' + titles.length);
-       
+
     // Get hrefs for all ads
     var hrefs_arr = this.getElementsAttribute('span.adTitle a.listlink', 'href');
     for (var i = 0; i < hrefs_arr.length; i++) {
         this.echo('hrefs_arr[' + i + ']: ' + hrefs_arr[i]);
     }
-    
+
     hrefs.extend(hrefs_arr);
     this.echo('hrefs.length after extend: ' + hrefs.length);
-   
-    
+
+
     // Get prices for all ads
     var prices_str = this.fetchText('div.priceBox');
     this.echo('prices_str: ' + prices_str);
@@ -139,7 +141,7 @@ function getMacInfo() {
     prices_arr = prices_arr.filter(function(item) {
                      return item.indexOf('$') !== -1;
                 });
-    
+
     this.echo('prices_arr.length: ' + prices_arr.length);
     for (var i = 0; i < prices_arr.length; i++) {
         this.echo('prices_arr[' + i + '] before formatting: ' + prices_arr[i]);
@@ -147,14 +149,14 @@ function getMacInfo() {
         prices_arr[i] = prices_arr[i].replace(',', '') // remove comma
         prices_arr[i] = (Number(prices_arr[i].slice(1)) / 100).toFixed(2);
         this.echo('prices_arr[' + i + ']: after formatting: ' + prices_arr[i]);
-    } 
-    
+    }
+
     prices.extend(prices_arr);
     this.echo('prices.length after extend: ' + prices.length);
-    
+
     var descriptions_arr = this.getElementsInfo('div.adDesc').map(function(e){
     return e.html;
-}); 
+});
     this.echo('descriptions_arr.length: ' + descriptions_arr.length);
     for (var i = 0; i < descriptions_arr.length; i++) {
         // remove everything starting with <a class="listlink" from description.
@@ -163,78 +165,33 @@ function getMacInfo() {
             descriptions_arr[i] = descriptions_arr[i].slice(0, a_loc);
         this.echo('descriptions_arr[' + i + ']: ' + descriptions_arr[i]);
     }
-    
+
     descriptions.extend(descriptions_arr);
-    this.echo('descriptions.length after extend: ' + descriptions.length); 
-} // getMacInfo
+    this.echo('descriptions.length after extend: ' + descriptions.length);
+
+    var nextPageSelector = 'a.pBox span.productPaginationButton';
+    if (this.visible(nextPageSelector)) {
+        this.echo('before next page click');
+        this.thenClick(nextPageSelector, getMacInfo) // recursively call getMacInfo
+    }
+}
 
 
 casper.start('http://www.ksl.com/index.php?nid=231&cat=554&category=16', function() { // KSL classifieds, Apple Laptops / Desktops
     this.echo('Page Title: ' + this.getTitle());
-    
+
     // Fill out keyword search field and submit
     this.sendKeys('input[name="keyword"]', 'Macbook pro');
-    this.click('input[id="searchSubmit"]');
-    this.echo('After keyword submit');
+    this.thenClick('input[id="searchSubmit"]', function() {
+        this.echo('After keyword submit');
+         // Fill out min and max search fields and submit (separate submit for price versus keyword. Keyword search still in place)
+        this.sendKeys('input[name="min_priceInput"]', '800');
+        this.sendKeys('input[name="max_priceInput"]', '1000');
+        this.echo('Before price submit');
+        this.thenClick('input[id="priceSubmit"]', getMacInfo);
+    });
 });
 
-// It happens when they change something...
-casper.evaluate(function(classname){
-  document.body.className += ' ' + classname;
-}, classname);
-
-casper.waitWhileSelector('body.' + classname, function() {
-
-    
-     // Fill out min and max search fields and submit (separate submit for price versus keyword. Keyword search still in place)
-    this.sendKeys('input[name="min_priceInput"]', '800');
-    this.sendKeys('input[name="max_priceInput"]', '1000');
-    this.echo('After price submit');
-    this.click('input[id="priceSubmit"]');
-}); 
-
-// It happens when they change something...
-casper.evaluate(function(classname){
-  document.body.className += ' ' + classname;
-}, classname);
-
-casper.waitWhileSelector('body.' + classname, getMacInfo);
-
-casper.then(function() {
-    
-    this.echo('before first clickNextPage call');
-    clickNextPage.call(this);
-    this.echo('after first clickNextPage call');
-    
-    // while (!donePaging) {  // infinite loop until timeout (no futher paging)
-        // It happens when they change something...
-        casper.evaluate(function(classname){
-          document.body.className += ' ' + classname;
-        }, classname);
-
-        casper.waitWhileSelector('body.' + classname,
-            function then() {
-                this.echo('before getMacInfo call');
-                getMacInfo.call(this);
-                this.echo('after getMacInfo call');
-                clickNextPage.call(this);
-                this.echo('after clickNextPage call');
-            },
-            function timeout() {
-                this.echo('in timeout. Set donePaging = true');
-                donePaging = true;
-            });
-
-   // } // while loop
-            
-    function clickNextPage() {
-        // Hit next page button to get next page of ads
-        this.echo('Before hitting next page button');
-        this.click('a.pBox span.productPaginationButton');
-        this.echo('After hitting next page button');
-    }
-});        
-    
 casper.then(function() {
  // Filter through title and description for Macbook Pro because original search filter isn't perfect. If no Macbook Pro in title or description remove from array.
     for (var i = titles.length - 1; i >= 0; i--) {
@@ -248,7 +205,7 @@ casper.then(function() {
             prices.splice(i, 1);
         }
     }
-    
+
     this.echo('titles.length after splice: ' + titles.length);
     this.echo('descriptions.length after splice: ' + descriptions.length);
     this.echo('hrefs.length after splice: ' + hrefs.length);
@@ -259,7 +216,7 @@ casper.then(function() {
         this.echo('hrefs[' + i + ']: ' + hrefs[i]);
         this.echo('prices[' + i + ']: ' + prices[i]);
     }
-    
+
     // Get detailed descriptions for all ads
     hrefs.forEach(function(elem, indx, arr) {
         casper.thenOpen('http://www.ksl.com/' + elem).then(function() {
@@ -268,11 +225,12 @@ casper.then(function() {
         });
     });
 });
-      
+
 casper.waitFor(function check() {
     return (detailedDescriptions.length === titles.length) && (titles.length !== 0);
 }, function then() {
-                                                  
+
+
     // Filter through title and description for hard drive size. Want >= 256GB. Handle all the different ways the ad author can specify hard drive size (xxxGB, xxxGigabyte, xxxgig, xxxterabyte, xxxSSD, or xxxHHD). Distinguish between hard drive and memory size by assuming > 32GB refers to hard drive. Delete all ads that don't meet criteria or don't specify hard drive size.
 
     this.echo('hard drive size search');
@@ -289,14 +247,14 @@ casper.waitFor(function check() {
                 this.echo('getHardDriveSizes call for detailedDesc');
                 hardDriveSizes[i] = getHardDriveSizes.call(this, detailedDescriptions[i]);
                 this.echo('hardDriveSizes[' + i + '] length (detailed descriptions): ' + hardDriveSizes[i].length);
-            }  
-        } 
+            }
+        }
     } // i loop
-    
+
     for (var i = titles.length - 1; i >= 0; i--) {
-        
+
         var bigEnough = false;
-    
+
         for (var j = 0; j < hardDriveSizes[i].length; j++) {
             if (hardDriveSizes[i][j] >= 256) { // big enough
                 bigEnough = true;
@@ -312,25 +270,25 @@ casper.waitFor(function check() {
             prices.splice(i, 1);
             detailedDescriptions.splice(i, 1);
         }
-            
+
     } // i loop
-    
-    
+
+
     // Write results to file
-    
+
     this.echo('titles.length: ' + titles.length);
     for (var i = 0; i < titles.length; i++) {
-        macbook_pros += 'title: ' + titles[i] + '\nprice: $' + prices[i] + '\nurl: ' + 'http://www.ksl.com/' + hrefs[i] + '\nbrief description: ' + descriptions[i] + '\ndetailed description: ' + detailedDescriptions[i] + '\n-------------------------------------------------------\n';
+        macbook_pros += 'title: ' + titles[i] + '\nprice: $' + prices[i] + '\nurl: ' + 'http://www.ksl.com/' + hrefs[i] + '\ndescription: ' + detailedDescriptions[i] + '\n-------------------------------------------------------\n';
     }
 
     this.echo('before write to file');
     var fs = require('fs');
     var date = new Date();
-    var fname = 'output/' + date.getFullYear() + '_' + (date.getMonth() + 1) + '_' + date.getDate() + '_' + date.getHours()  + date.getMinutes() + '.txt';
+    var fname = 'output/' + date.getFullYear() + '_' + (date.getMonth() + 1) + '_' + date.getDate() + '_' + addZero.call( this, date.getHours())  + addZero.call(this, date.getMinutes()) + '.txt';
     fs.write(fname, macbook_pros, 'w');
     this.echo('after write to file');
-    
-    
+
+
 }, function timeout() {
     this.echo('timed out waiting ').exit();
 }, 600000);  // increase timeout from default (5 secs) to 600 secs
